b56b738d554c350725284de39ad9587adc6617cc
Nathan Walker <75284703+nwalker555@users.noreply.github.com>
2021-05-30 21:52:30 -0400

    Note Taker completed

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const express = require ('express');
+const database = require("./db/db.json");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+require('../develop/routes/routes.js')(app)
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("./notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
