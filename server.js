236c2b0ac0c80b7b9a74da7b8e2d9cad8c7e8013
Nathan Walker <75284703+nwalker555@users.noreply.github.com>
2021-06-03 20:54:13 -0400

    damn you heroku

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const express = require ('express');
 const database = require("./db/db.json");
-console.log(database);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -16,7 +15,7 @@ app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.get("./notes", function (req, res) {
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/notes.html"));
 });
 
