e3f95376f5882fbb460e640ba038219333c068c8
Nathan Walker <75284703+nwalker555@users.noreply.github.com>
2021-06-03 20:15:27 -0400

    got a tutor thank baby jesus

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const express = require ('express');
 const database = require("./db/db.json");
+console.log(database);
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-require("./Routes/routes")(app)
+require("./routes/routes")(app)
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"));
